3510de350704655873b776ce2d2550c1840fdcdd
Thiago <desenvolvimento3@mediaplus.com.br>
2017-06-27 09:19:59 -0300

    alteração

--- home.php
+++ home.php
@@ -1,25 +1,5 @@
 <?php include "template-site/header.php"; ?>
 <?php include "template-site/top.php"; ?>
 
-<ul class="tabs" data-responsive-accordion-tabs="tabs medium-accordion" id="asdasdasd">
-    <li class="tabs-title is-active"><a href="#panel1c" aria-selected="true">Tab 1</a></li>
-    <li class="tabs-title"><a href="#panel2c">Tab 2</a></li>
-    <li class="tabs-title"><a href="#panel3c">Tab 3</a></li>
-    <li class="tabs-title"><a href="#panel4c">Tab 4</a></li>
-</ul>
 
-<div class="tabs-content" data-tabs-content="asdasdasd">
-    <div class="tabs-panel is-active" id="panel1c">
-        <p>Conteudo 1</p>
-    </div>
-    <div class="tabs-panel" id="panel2c">
-        <p>Conteudo 2</p>
-    </div>
-    <div class="tabs-panel" id="panel3c">
-        <p>Conteudo 3</p>
-    </div>
-    <div class="tabs-panel" id="panel4c">
-        <p>Conteudo 4</p>
-    </div>
-</div>
 <?php include "template-site/footer.php"; ?>
