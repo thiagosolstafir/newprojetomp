21577c4ae559bdbcacfb95ed3381a0418d7b471c
Thiago <desenvolvimento3@mediaplus.com.br>
2017-06-27 08:53:21 -0300

    initial commit

---
+++ template-site/footer.php
@@ -0,0 +1,21 @@
+<script src="<?php echo $mainFolder; ?>/includes/js/iscroll.js"></script>
+<script src="<?php echo $mainFolder; ?>/includes/js/swiper.js"></script>
+
+<script>
+    //foundation
+    $(document).foundation();
+</script>
+
+</div>
+</div>
+</div>
+
+<!-- load lightbox default -->
+<div id="load">
+    <span></span>
+    <div></div>
+</div>
+
+
+</body>
+</html>
---
+++ template-site/header.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html class="no-js" lang="pt-br">
+<head>
+    <meta charset="iso-8859-1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport"
+    content="target-densitydpi=device-dpi, width=device-width, user-scalable=no, maximum-scale=1, minimum-scale=1"/>
+
+    <title><?php echo $siteName; ?></title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo $mainFolder; ?>/includes/css/app.css?<?php echo rand(); ?>">
+    <link rel="stylesheet" href="<?php echo $mainFolder; ?>/includes/css/mobile.css?<?php echo rand(); ?>">
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/wow.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/main.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.cycle2.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.cycle2.carousel.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.cycle2.swipe.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.cycle2.center.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.cycle2.caption2.js?<?php echo rand(); ?>"></script>
+    <script src="<?php echo $mainFolder; ?>/includes/js/jquery.cycle2.scrollVert.js?<?php echo rand(); ?>"></script>
+
+    <!-- Compressed JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.min.js" integrity="sha256-Nd2xznOkrE9HkrAMi4xWy/hXkQraXioBg9iYsBrcFrs=" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+    $.fn.cycle.defaults.autoSelector = '.slideshow';
+    new WOW().init();
+
+    window.mainFolder = '<?php echo $mainFolder; ?>';
+    window.idMP = '';
+    window.descMP = '';
+    window.mpPage = '<?php echo ucfirst($page); ?>';
+    window.mainTitle = '<?php echo $siteName; ?>';
+    </script>
+</head>
+<body>
+
+
+    <div class="m-mobile off-canvas position-left" id="menuMobile" data-off-canvas>
+
+        <button class="close-button" aria-label="Close menu" type="button" data-close>
+            <span aria-hidden="true">&times;</span>
+        </button>
+
+        <!-- Menu mobile -->
+
+
+    </div>
+
+    <div class="off-canvas-content" data-off-canvas-content>
+
+        <div id="lt" class="fluid">
+            <div id="lt-int"></div>
+        </div>
+
+        <div id="lt-scroll" class="fluid">
+            <div id="lt-scroll-int"></div>
+        </div>
