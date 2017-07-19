f0fc3d1c511dc878c0d717a80b618a2b3a7dd77c
Thiago <desenvolvimento3@mediaplus.com.br>
2017-07-19 17:27:39 -0300

    alteracao

--- home.php
+++ home.php
@@ -1,5 +1,12 @@
 <?php include "template-site/header.php"; ?>
 <?php include "template-site/top.php"; ?>
 
+<div class="" style="width: 500px; height:auto; float:none; display:table; margin: 0 auto;">
+<button class="button" type="button" data-toggle="asiduhsaodjai">asodjaisojdoasi</button>
+<div class="dropdown-pane" id="asiduhsaodjai" data-dropdown data-hover="false" data-hover-pane="true">
+    asidjaiosjdiasd
+</div>
+</div>
+
 
 <?php include "template-site/footer.php"; ?>
