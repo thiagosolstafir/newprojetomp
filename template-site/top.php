8d4d31bace6fd18ca7b741e1f3e8641df1eb7d33
Thiago <desenvolvimento3@mediaplus.com.br>
2017-07-31 11:51:15 -0300

    alteracao

--- template-site/top.php
+++ template-site/top.php
@@ -1,7 +1,9 @@
 <div class="title-bar">
     <div class="title-bar-left">
         <button class="menu-icon" type="button" data-open="menuMobile"></button>
-        <span class="title-bar-title"><?php echo $siteName; ?></span>
+    </div>
+    <div class="title-bar-right">
+       <span class="title-bar-title"><?php echo $siteName; ?></span>
     </div>
 </div>
 
