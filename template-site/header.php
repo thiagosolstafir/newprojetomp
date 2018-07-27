62132d7dba97910563112e345bb77889de74ac4e
Thiago <desenvolvimento3@mediaplus.com.br>
2018-07-27 09:34:56 -0300

    ajts

--- template-site/header.php
+++ template-site/header.php
@@ -4,9 +4,11 @@
   <meta charset="iso-8859-1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <meta name="viewport"
-        content="target-densitydpi=device-dpi, width=device-width, user-scalable=no, maximum-scale=1, minimum-scale=1"/>
+  <meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, user-scalable=no, maximum-scale=1, minimum-scale=1"/>
 
+  <link rel="icon" type="image/png" href="<?php echo $mainFolder; ?>/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="<?php echo $mainFolder; ?>/favicon-16x16.png" sizes="16x16" />
+  
   <title><?php echo $siteName; ?></title>
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
   <link href="<?php echo $mainFolder; ?>/includes/css/lightgallery.css" rel="stylesheet">
@@ -34,13 +36,14 @@
   <link rel="stylesheet" type="text/css" href="<?php echo $mainFolder; ?>/includes/css/fallback.css"/>
   <link rel="stylesheet" type="text/css" href="<?php echo $mainFolder; ?>/includes/lib/jquery.sweetalert2.css"/>
   <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/jquery.sweetalert2.js"></script>
+  <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/lib/jquery.form.js"></script>
   <script type="text/javascript" src="<?php echo $mainFolder; ?>/includes/js/mp-form-ajax.js"></script>
 
   <script type="text/javascript">
     new WOW().init();
 
     window.mainFolder = '<?php echo $mainFolder; ?>';
-    window.mpPage = '<?php echo ucfirst($page); ?>';
+    window.mpPage = '<?php echo ucfirst(isset($page)); ?>';
     window.mainTitle = '<?php echo $siteName; ?>';
   </script>
 </head>
@@ -58,7 +61,7 @@
   <!--        </div>-->
 
   <ul class="vertical menu">
-    <li><a href="<?php echo $mainFolder; ?>/home" <?php if ($page == "home") { ?> class="ativo" <?php } ?>>Home</a></li>
+    <li><a href="<?php echo $mainFolder; ?>/home" <?php if (isset($page) == "home") { ?> class="ativo" <?php } ?>>Home</a></li>
   </ul>
 
 </div>
